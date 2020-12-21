00e322bd5c6758afc157d9a913bed5b773baa37c
mcnamacl <mcnamacl@tcd.ie>
2020-12-21 18:12:56 +0000

    Remove LinearSVR and added kfold for Lasso

--- model_training.py
+++ model_training.py
@@ -7,7 +7,6 @@ from sklearn.model_selection import KFold, train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn.dummy import DummyRegressor 
 from sklearn.impute import SimpleImputer
-from sklearn.svm import LinearSVR
 from sklearn.linear_model import Lasso
 
 # Stops dividing by zero kNN error.
@@ -34,58 +33,29 @@ def plotMeanAndStdDev(xplot, xlabel, means, devs, title):
 def trainWithCombinationsLasso(x, y):
     cValues = [1, 10, 100, 1000, 10000]
     means = []
-    print("Lasso Parameters:")
-
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+    devs = []
+    kf = KFold(n_splits=5)
 
     for c in cValues:
-        model = Lasso(alpha=(1/(2*c))).fit(X_train, y_train)
-        print("C=", c)
-        # Get the parameter values of the trained model.
-        print(model.coef_)
-        print(model.intercept_)
-
-        # Use the model to generate predictions.
-        newPredictions = model.predict(X_test)
-
-        mean = mean_squared_error(y_test, newPredictions)
-        means.append(mean)
-
-        print("Mean Lasso " + str(c))
-        print(mean_squared_error(y_test, newPredictions))
-
-    # Plot the mean and standard deviation of various C values.
-    plotMeanAndStdDev(np.array(cValues), "C", means, [0,0,0,0,0], "Lasso Cross Validation C")  
-
-# Cross validation for various values of C using a KernelRidge model.
-def crossValidationLinearSVR(x, y):
-    means = []
-    devs = []
-    C = [0.01, 0.1, 1, 10, 25]
-    # Use 5 folds.
-    kf = KFold(n_splits=5)
-    for c in C:
         tmpMeanErrors = []
         for train, test in kf.split(x):
-            model = LinearSVR(C=c).fit(x[train], y[train])
-            ypred = model.predict(x[test])
+            model = Lasso(alpha=(1/(2*c))).fit(x[train], y[train])
 
-            mask = np.isnan(ypred)
-            idx = np.where(~mask,np.arange(mask.size),0)
-            np.maximum.accumulate(idx, out=idx)
-            ypred = ypred[idx]
+            # Use the model to generate predictions.
+            newPredictions = model.predict(x[test])
 
-            # Gets the mean squared error from the predictions vs. the target values.
-            predError = mean_squared_error(y[test],ypred)
-            # Store the mean squared error.
-            tmpMeanErrors.append(predError)
+            mean = mean_squared_error(y[test], newPredictions)
+            tmpMeanErrors.append(mean)
+
+        print("Mean Lasso " + str(c))
+        print(np.mean(tmpMeanErrors))
         # Get the mean of the mean squared errors.
         means.append(np.mean(tmpMeanErrors))
         # Get the standard deviation of the mean squared errors.
         devs.append(np.std(tmpMeanErrors))
-    print("Cross validation c: ", means)
-    # Plot the mean and standard deviation of various C valies.
-    plotMeanAndStdDev(np.array(C), "C", means, devs, "LinearSVR Cross Validation C")  
+
+    # Plot the mean and standard deviation of various C values.
+    plotMeanAndStdDev(np.array(cValues), "C", means, devs, "Lasso Cross Validation C")  
 
 # Cross validation for various values of gamma using a kNN model.
 def crossValidationkNN(x, y):
@@ -167,12 +137,10 @@ def testEachModel(X, y, X_test, y_test):
     lasso_predError = mean_squared_error(lasso_preds, y_test)
     print("Lasso mean squared error: ", lasso_predError)
 
-    linearSVR_model = LinearSVR(C=10, max_iter=10000).fit(X, y)
-    linearSVR_preds = linearSVR_model.predict(X_test)
-    linearSVR_predError = mean_squared_error(linearSVR_preds, y_test)
-    print("linearSVR mean squared error: ", linearSVR_predError)
-
-    gIndex = 1
+    global gIndex
+    global gamma
+    gamma = [1]
+    gIndex = 0
     kNN_model = KNeighborsRegressor(n_neighbors=len(X), weights=gaussian_kernel).fit(X, y)
     kNN_preds = kNN_model.predict(X_test)
     kNN_predError = mean_squared_error(kNN_preds, y_test)
@@ -217,13 +185,11 @@ if __name__ == "__main__":
 
     X_joined = np.column_stack((X))
 
-    # testLinearRegression(X_joined, y)
+    testLinearRegression(X_joined, y)
 
-    # crossValidationkNN(X_joined, y)
+    crossValidationkNN(X_joined, y)
 
-    # crossValidationLinearSVR(X_joined, y)
-
-    # trainWithCombinationsLasso(X_joined, y)
+    trainWithCombinationsLasso(X_joined, y)
 
     X_test, y_test = readDataset("original_data_2018.csv")
 
